2004db37a4a859686ccf81e078fab82bb81e7fb1
Pedro Martins <pedro@shortcut.pt>
2018-11-13 04:02:33 +0000

    Configured WebAPI endpoint URL

--- DeliveryServiceExercise/DeliveryServiceExercise.Web/Controllers/HomeController.cs
+++ DeliveryServiceExercise/DeliveryServiceExercise.Web/Controllers/HomeController.cs
@@ -1,15 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Configuration;
 using System.Web.Mvc;
 
 namespace DeliveryServiceExercise.Web.Controllers
 {
     public class HomeController : Controller
     {
+        [OutputCache(Duration = 60)]
         public ActionResult Index()
         {
+            ViewData["apiURL"] = ConfigurationManager.AppSettings.Get("DeliveryServiceAPIUrl");
             return View();
         }
     }
